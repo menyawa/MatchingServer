e1a78ec1b6b970f0b93333260272d3b38aaecf88
menyawa <41637976+menyawa@users.noreply.github.com>
2021-06-23 11:15:40 +0900

    ・Roomにカスタムプロパティ、開いているかどうか、最大プレイヤー数の概念を作成 ・満室なら入室できないよう修正

--- MatchingServer/Source/Room.cs
+++ MatchingServer/Source/Room.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace MatchingServer {
@@ -8,24 +9,33 @@ namespace MatchingServer {
     class Room {
         //現在部屋にいるプレイヤー
         private readonly List<Player> PLAYERS = new List<Player>();
+        //ルームに紐付く何らかのパラメータ
+        //パラメータごとに値の型は変わる可能性があるため、DictionaryでなくHashtableで作成
+        private readonly Hashtable CUSTOM_PROPERTIES = new Hashtable();
+
+        //開いているかどうか
+        public bool isOpen_ = true;
+        //最大何人まで入室できるのか
+        private readonly int MAX_PLAYER_COUNT;
 
         /// <summary>
         /// ルームのみを作成する場合のコンストラクタ
         /// </summary>
-        public Room() {}
+        public Room(int maxPlayerCount) : this(new string[]{}, maxPlayerCount) {}
 
         /// <summary>
         /// 作成と同時に1人入室する際のコンストラクタ
         /// </summary>
         /// <param name="id"></param>
-        public Room(string id) : this(new string[]{ id }) {}
+        public Room(string id, int maxPlayerCount) : this(new string[]{ id }, maxPlayerCount) {}
 
         /// <summary>
         /// 作成と同時に何人か入室する際のコンストラクタ
         /// </summary>
         /// <param name="ids"></param>
-        public Room(string[] ids) {
+        public Room(string[] ids, int maxPlayerCount) {
             foreach(var id in ids) join(id);
+            MAX_PLAYER_COUNT = maxPlayerCount;
         }
 
         /// <summary>
@@ -45,8 +55,9 @@ namespace MatchingServer {
         /// <param name="player"></param>
         /// <returns></returns>
         public Player join(Player player) {
-            //渡されたPlayerがnullなら何もせず返す
+            //渡されたPlayerがnull、または既に満員なら何もせず返す
             if (player == null) return null;
+            if (PLAYERS.Count() == MAX_PLAYER_COUNT) return null;
 
             PLAYERS.Add(player);
             return player;
