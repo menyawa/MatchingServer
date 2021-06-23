0ac8479762b70c3fdd05a2f3e5c3acfad6216ed1
menyawa <41637976+menyawa@users.noreply.github.com>
2021-06-23 14:29:23 +0900

    ・クラスLobbyを作成

---
+++ MatchingServer/Source/Lobby.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MatchingServer {
+    /// <summary>
+    /// 対戦ロビーのクラス
+    /// </summary>
+    sealed class Lobby : ElementsBase {
+        private readonly List<Room> ROOMS = new List<Room>();
+
+        public Lobby() { }
+    }
+}
