fe051c445045f430f7de10c6fae31e558b0dde40
menyawa <41637976+menyawa@users.noreply.github.com>
2021-06-24 13:51:06 +0900

    ・サーバ・クライアント間で送受信するメッセージの構造体MessageDataを追加

---
+++ MatchingServer/Source/MessageData.cs
@@ -0,0 +1,27 @@
+﻿namespace MatchingServer {
+    /// <summary>
+    /// サーバ・クライアント間で送受信するメッセージのデータ
+    /// </summary>
+    struct MessageData {
+        //プレイヤーのIDとニックネーム
+        public readonly string PLAYER_ID;
+        public readonly string PLAYER_NICK_NAME;
+        //希望するプレイ人数
+        public readonly int MAX_PLAYER_COUNT;
+
+        //新しくルームに入りたいのか(対戦を行いたいのか)、今いるルームから退出したい(接続は切らない)のか、切断するのか
+        public enum Type {
+            Join, 
+            Leave, 
+            Disconnect
+        }
+        public Type type_;
+
+        public MessageData(string id, string nickName, int maxPlayerCount, Type type) {
+            PLAYER_ID = id;
+            PLAYER_NICK_NAME = nickName;
+            MAX_PLAYER_COUNT = maxPlayerCount;
+            type_ = type;
+        }
+    }
+}
