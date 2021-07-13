9a7d6cc6752afdc1b6cc218ddcb056ce022c815b
menyawa <41637976+menyawa@users.noreply.github.com>
2021-07-13 10:25:08 +0900

    ・デバッグ用出力をDebug.WriteLineを用いる方式に修正

--- MatchingServer/Source/Program.cs
+++ MatchingServer/Source/Program.cs
@@ -1,16 +1,21 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace MatchingServer {
     class Program {
         private static async Task Main(string[] args) {
+            //デバッグ出力が標準出力に(コンソールに)出力されるようにする
+            Trace.Listeners.Add(new ConsoleTraceListener());
+            Debug.WriteLine("Debug Mode");
+
             while (true) {
                 //クライアントの接続を受け入れ次第次の受け入れを開始する
                 //awaitで止まるため、接続が行われないまま次の受け入れが始まる心配はない
                 //受け入れがエラーとなって終わった場合nullが返されるので、その場合次に行く
                 var webSocket = await Server.acceptClientConnecting();
                 if(webSocket != null) Server.RunAsync(webSocket);
-                Console.WriteLine("Next Standby");
+                Debug.WriteLine("Next Standby");
 
 
                 //ゲームアプリ→クライアントアプリ→サーバへのメッセージ伝送と、その逆を検証する
--- MatchingServer/Source/Server.cs
+++ MatchingServer/Source/Server.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.WebSockets;
@@ -23,7 +24,7 @@ namespace MatchingServer {
 
         static Server() {
             //Httpリスナーを立ち上げておく(接続待ちは別で行う)
-            Console.WriteLine("HttpListener Init");
+            Debug.WriteLine("HttpListener Init");
             HTTP_LISTENER = new HttpListener();
             HTTP_LISTENER.Prefixes.Add("http://localhost:8000/ws/");
             HTTP_LISTENER.Start();
@@ -43,7 +44,7 @@ namespace MatchingServer {
             int playerID = INVAID_ID;
             int currentRoomIndex = INVAID_ID;
             //受信中の応答無しの時間を測定する関係上、awaitは使わない
-            var clientMessageTask = getReceiveMessageAsync(webSocket);
+            var getClientMessageTask = getReceiveMessageAsync(webSocket);
             while (webSocket.State != WebSocketState.Closed && webSocket.State != WebSocketState.CloseReceived) {
                 //毎フレーム必ず更新を行う
                 //最後でなく最初に更新を行わないと、途中でcontinueを行った場合更新が行われないので注意
@@ -54,21 +55,21 @@ namespace MatchingServer {
 
                 //メッセージの受信完了したら新たなメッセージの受信待ちを開始し、応答無しの累計時間をリセットする
                 var clientMessageData = MessageData.getBlankData();
-                if (clientMessageTask.IsCompletedSuccessfully) {
+                if (getClientMessageTask.IsCompletedSuccessfully) {
                     //ここで受信データを入れておかないと、この後では既に新しい待受けのタスクに変わってしまっているため、正常にメッセージを受信できないことに注意
-                    clientMessageData = JsonSerializer.Deserialize<MessageData>(await clientMessageTask);
+                    clientMessageData = JsonSerializer.Deserialize<MessageData>(await getClientMessageTask);
                     
                     clientMessageData.printInfo();
                     
-                    clientMessageTask = getReceiveMessageAsync(webSocket);
+                    getClientMessageTask = getReceiveMessageAsync(webSocket);
                     noResponseTime = 0;
                 } else {
                     //完了していないなら応答なしの時間を加算し、タイムアウト次第切断する
                     //まだタイムアウトしないなら次のループへ
                     noResponseTime += deltaTimer.get();
-                    Console.WriteLine($"No Responce Time: {noResponseTime}");
+                    Debug.WriteLine($"No Responce Time: {noResponseTime}");
                     if (isTimeOut(noResponseTime)) {
-                        Console.WriteLine("Time Out");
+                        Debug.WriteLine("Time Out");
                         await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Time Out", CancellationToken.None);
                         break;
                     } else continue;
@@ -82,12 +83,12 @@ namespace MatchingServer {
         /// クライアントのアクセスを受け入れ、WebSOcketを返す
         /// </summary>
         public static async Task<WebSocket> acceptClientConnecting() {
-            Console.WriteLine("Accept WebSocket Standby");
+            Debug.WriteLine("Accept WebSocket Standby");
             var httpListenerContext = await HTTP_LISTENER.GetContextAsync();
 
             //クライアントからのリクエストがWebSocketでないなら閉じてnullを返す
             if (httpListenerContext.Request.IsWebSocketRequest == false) {
-                Console.WriteLine("Error: Request is Not WebSocket");
+                Debug.WriteLine("Error: Request is Not WebSocket");
                 httpListenerContext.Response.StatusCode = 400;
                 httpListenerContext.Response.Close();
                 return null;
@@ -105,6 +106,7 @@ namespace MatchingServer {
         /// <param name="message"></param>
         /// <returns></returns>
         private static void sendMessage(WebSocket webSocket, string message) {
+            Debug.WriteLine($"Send Client to {message}");
             //文字列をバイト列に変換して送る
             var buffer = ENCODING.GetBytes(message);
             var segment = new ArraySegment<byte>(buffer);
@@ -154,7 +156,9 @@ namespace MatchingServer {
                 count += result.Count;
             }
 
-            return ENCODING.GetString(buffer, 0, count);
+            string messageStr = ENCODING.GetString(buffer, 0, count);
+            Debug.WriteLine($"Get message of {messageStr}");
+            return messageStr;
         }
 
         /// <summary>
@@ -163,6 +167,7 @@ namespace MatchingServer {
         /// <param name="webSocket"></param>
         /// <param name="player"></param>
         public static void sendPlayerDataToClient(WebSocket webSocket, Player player, int maxPlayerCount, MessageData.Type type) {
+            Debug.WriteLine("Send Player Data");
             var messageData = new MessageData(player.ID, player.NICK_NAME, maxPlayerCount, type);
             sendMessage(webSocket, JsonSerializer.Serialize(messageData));
         }
